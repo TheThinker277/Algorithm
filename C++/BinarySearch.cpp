29ad34fa57e568b9937ddee36d1031865643f02d
TheThinker277 <f20191482@hyderabad.bits-pilani.ac.in>
2021-06-21 14:13:12 +0530

    EDIT: Added  BS using iteration with dynamic input

--- C++/BinarySearch.cpp
+++ C++/BinarySearch.cpp
@@ -1,7 +1,25 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-
-int binarySearch(int arr[], int l, int r, int key) 
+#include<iostream>
+using namespace std;
+//using iteration
+int binarySearchIter(int a[], int l, int r, int key) 
+{ 
+   while(r>l) { 
+        int mid = l + (r - l) / 2; 
+  
+        if (a[mid] == key) 
+            return mid; 
+   
+        if (a[mid] > key) 
+            r=mid-1;
+        else
+            l=mid+1;
+         
+    } 
+   
+    return -1; 
+} 
+//using recursion 
+int binarySearchRec(int arr[], int l, int r, int key) 
 { 
     if (r >= l) { 
         int mid = l + (r - l) / 2; 
@@ -10,21 +28,32 @@ int binarySearch(int arr[], int l, int r, int key)
             return mid; 
    
         if (arr[mid] > key) 
-            return binarySearch(arr, l, mid - 1, key); 
+            return binarySearchRec(arr, l, mid - 1, key); 
   
-        return binarySearch(arr, mid + 1, r, key); 
+        return binarySearchRec(arr, mid + 1, r, key); 
     } 
    
     return -1; 
 } 
-  
-int main(void) 
-{ 
-    int arr[] = { 23,45,67,87,98 }; 
-    int key = 98; 
-    int n = sizeof(arr) / sizeof(arr[0]); 
-    int pos = binarySearch(arr, 0, n - 1, key); 
+int main()
+{
+
+    int n,key;
+    cout<<"ENTER THE NUMBER OF ELEMENTS"<<"\n";
+    cin>>n;
+    cout<<"ENTER THE ELEMENTS"<<"\n";
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<"ENTER THE ELEMENT YOU WANT TO FIND THE POSITION OF"<<"\n";
+    cin>>key;  
+    int pos = binarySearchIter(a, 0, n - 1, key);
+    int pos2 = binarySearchRec(a, 0, n - 1, key);  
     (pos == -1) ? cout << "Element is not present in array"
-                   : cout << "Element is present at position " << pos+1; 
+                   : cout << "Element is present at position " << pos+1<<endl;
+    (pos2 == -1) ? cout << "Element is not present in array"
+                   : cout << "Element is present at position " << pos2+1;                 
     return 0; 
-} 
+}
